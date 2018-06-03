d6fe78cc3b86225088ce863b339ba2cede6a37b4
中嶋敏詞 <nakajimasatoshi@nakajimasatoshishi-no-MacBook-Air.local>
2018-06-03 16:24:20 +0900

    add: contents

--- script.js
+++ script.js
@@ -1,32 +1,59 @@
 $(function () {
-   $(window).on('scroll', function() {
+    $(window).on('scroll', function () {
         var scroll_top = $(window).scrollTop();
-       if (scroll_top > -10){
-           $('.right_box').addClass('show');
-       }else{
-           $('.right_box').removeClass('show');
-       }
-   }); 
+        if (scroll_top > 0.1) {
+            $('.right_box').addClass('show');
+        } else {
+            $('.right_box').removeClass('show');
+        }
+    });
 });
 
 $(function () {
-   $(window).on('scroll', function() {
+    $(window).on('scroll', function () {
         var scroll_top = $(window).scrollTop();
-       if (scroll_top > 500){
-           $('.left_box2').addClass('show');
-       }else{
-           $('.left_box2').removeClass('show');
-       }
-   }); 
+        if (scroll_top > 0.1) {
+            $('.left_box').addClass('show');
+        } else {
+            $('.left_box').removeClass('show');
+        }
+    });
 });
 
 $(function () {
-   $(window).on('scroll', function() {
+    $(window).on('scroll', function () {
         var scroll_top = $(window).scrollTop();
-       if (scroll_top > 1000){
-           $('.left_box3').addClass('show');
-       }else{
-           $('.left_box3').removeClass('show');
-       }
-   }); 
+        if (scroll_top > 500) {
+            $('.left_box2').addClass('show');
+        } else {
+            $('.left_box2').removeClass('show');
+        }
+    });
+});
+
+$(function () {
+    $(window).on('scroll', function () {
+        var scroll_top = $(window).scrollTop();
+        if (scroll_top > 900) {
+            $('.left_box3').addClass('show');
+        } else {
+            $('.left_box3').removeClass('show');
+        }
+    });
+});
+
+$(function () {
+    $('.box img').addClass('move');
+    $(window).scroll(function () {
+        $(".box").each(function () {
+            var imgPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > imgPos - windowHeight + windowHeight / 6) {
+                $(this).find("img").addClass('move');
+            } else {
+                $(this).find("img").removeClass('move');
+            }
+        });
+    });
 });
